8fd315cefb1834e12aa06453ac3a43d906e692d9
Philip Simonson <psimonson1988@gmail.com>
2019-06-21 10:54:41 -0400

    Redone the print_object function.

--- main.c
+++ main.c
@@ -10,12 +10,34 @@
 
 static struct objfile *obj;
 
+void change_size(int w, int h)
+{
+	float ratio;
+
+	if(h == 0)
+		h = 1;
+
+	ratio = 1.0*w/h;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(45, ratio, 1, 100);
+	glMatrixMode(GL_MODELVIEW);
+}
+
 void render_scene(void)
 {
 	extern struct objfile *obj;
 
+	glClearColor(0.0f, 0.0f, 0.7f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
 
+	gluLookAt(0.0f, 0.0f, 10.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+	
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(vector_size(obj->vertices), GL_FLOAT, 0, obj->vertices);
 	glDisableClientState(GL_VERTEX_ARRAY);
@@ -31,6 +53,8 @@ int init_glut(int argc, char **argv)
 	glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
 	if(!glutCreateWindow("OBJFILE v0.01")) return 1;
 	glutDisplayFunc(render_scene);
+	glutIdleFunc(render_scene);
+	glutReshapeFunc(change_size);
 	return 0;
 }
 
@@ -43,6 +67,7 @@ int main(int argc, char **argv)
 
 	obj = load_object("test.obj");
 	if(!obj) return 1;
+	print_object(obj);
 	glutMainLoop();
 	destroy_object(obj);
 	return 0;
--- object.c
+++ object.c
@@ -64,7 +64,27 @@ struct objfile *load_object(const char *filename)
  */
 void print_object(struct objfile *obj)
 {
-	UNUSED(obj);
+	size_t i;
+	printf("Below is all the data... (Vertices, Normals, Faces).\n"
+		"In that order.\n"
+		"=====================================================\n");
+	for(i=0; i < vector_size(obj->vertices); i+=3) {
+		printf("%f %f %f\n", obj->vertices[i],
+			obj->vertices[i+1], obj->vertices[i+2]);
+	}
+	printf("=====================================================\n");
+	for(i=0; i < vector_size(obj->vertices); i+=3) {
+		printf("%f %f %f\n", obj->vertices[i],
+			obj->vertices[i+1], obj->vertices[i+2]);
+	}
+	printf("=====================================================\n");
+	for(i=0; i < vector_size(obj->faces); i+=8) {
+		printf("%d//%d %d//%d %d//%d %d//%d\n", obj->faces[i],
+			obj->faces[i+1], obj->faces[i+2],
+			obj->faces[i+3], obj->faces[i+4],
+			obj->faces[i+5], obj->faces[i+6],
+			obj->faces[i+7]);
+	}
 }
 /**
  * @brief Destroy given object structure.
